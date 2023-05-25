cd3b8dfacba38079032886e0a62f872ef7fdf5f5
Houssem Nasri <housi.housi2015@gmail.com>
2023-05-25 12:00:09 +0100

    Display register items in a listview

--- GCE/src/main/java/tech/houssemnasri/gce/ui/RegistersViewer.java
+++ GCE/src/main/java/tech/houssemnasri/gce/ui/RegistersViewer.java
@@ -1,51 +1,75 @@
 package tech.houssemnasri.gce.ui;
 
 import javafx.beans.InvalidationListener;
-import javafx.geometry.Insets;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 import tech.houssemnasri.gce.Data;
 import tech.houssemnasri.gce.Register;
 import tech.houssemnasri.gce.RegisterFile;
 
-public class RegistersViewer extends ScrollPane {
+public class RegistersViewer extends AnchorPane implements Callback<ListView<RegistersViewer.RegisterItemModel>, ListCell<RegistersViewer.RegisterItemModel>> {
     private final RegisterFile registerFile;
-    private final VBox root;
+    private final ListView<RegisterItemModel> listView;
 
     public RegistersViewer(RegisterFile registerFile) {
         this.registerFile = registerFile;
+        this.listView = new ListView<>();
+        listView.setCellFactory(this);
 
+        AnchorPane.setLeftAnchor(listView, 0d);
+        AnchorPane.setRightAnchor(listView, 0d);
+        AnchorPane.setTopAnchor(listView, 0d);
+        AnchorPane.setBottomAnchor(listView, 0d);
 
-        this.root = new VBox();
-        root.setPadding(new Insets(24, 8, 24, 8));
-        root.setSpacing(12);
-        this.setContent(root);
+        getChildren().setAll(listView);
 
-        updateRegistersUI(registerFile);
-        registerFile.getRegisterList().addListener((InvalidationListener) e -> {
-            updateRegistersUI(registerFile);
-        });
+        updateRegisters();
+        registerFile.getRegisterList().addListener((InvalidationListener) e -> updateRegisters());
     }
 
-    private void updateRegistersUI(RegisterFile registerFile) {
-        root.getChildren().clear();
-        for (int regIndex = 0; regIndex < RegisterFile.SIZE; regIndex++) {
-            Register register = Register.byIndex(regIndex);
-            int value = registerFile.readRegister(register);
-            root.getChildren().add(createRegisterItem(register, Data.fromInt(value)));
+    private void updateRegisters() {
+        listView.getItems().clear();
+        ObservableList<RegisterItemModel> registerItems = FXCollections.observableArrayList();
+        for (int i = 0; i < RegisterFile.SIZE; i++) {
+            RegisterItemModel registerItemModel = new RegisterItemModel();
+            registerItemModel.register = Register.byIndex(i);
+            registerItemModel.value = Data.fromInt(registerFile.readRegister(registerItemModel.register));
+            registerItems.add(registerItemModel);
         }
+        listView.getItems().setAll(registerItems);
     }
 
-    public HBox createRegisterItem(Register register, Data value) {
-        HBox container = new HBox(8);
-        Label registerNameLabel = new Label(String.format("%s (x%d)", register.name(), register.index()));
-        registerNameLabel.setMinWidth(120);
-        TextField registerValueTextField = new TextField(value.hex());
-        registerValueTextField.setEditable(false);
-        container.getChildren().addAll(registerNameLabel, registerValueTextField);
-        return container;
+    @Override
+    public ListCell<RegisterItemModel> call(ListView<RegisterItemModel> param) {
+        return new ListCell<RegisterItemModel>() {
+            @Override
+            protected void updateItem(RegisterItemModel item, boolean empty) {
+                super.updateItem(item, empty);
+                if (!empty && item != null) {
+                    // Workaround to use a custom layout for the cell
+                    setText("");
+                    setGraphicTextGap(0);
+
+                    Label label = new Label(String.format("%s (x%d)", item.register.name(), item.register.index()));
+                    label.setMinWidth(120);
+                    TextField textfield = new TextField(item.value.hex());
+                    textfield.setEditable(false);
+                    HBox hbox = new HBox(12, label, textfield);
+                    setGraphic(hbox);
+                }
+            }
+        };
+    }
+
+    static class RegisterItemModel {
+        private Register register;
+        private Data value;
     }
 }
