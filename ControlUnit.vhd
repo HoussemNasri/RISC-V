9a88778fcb734f9caefb087a5e5afa191e6ea9a4
Houssem Nasri <housi.housi2015@gmail.com>
2023-05-26 15:02:21 +0100

    Implement the 'bne' instruction

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -19,40 +19,15 @@ entity ControlUnit is
 end;
 
 architecture Behavioural of ControlUnit is
-	component MainDecoder is 
-		port(
-			opcode: in std_logic_vector(6 downto 0);
-			RegWrite: out std_logic;
-			ImmSrc: out std_logic_vector(1 downto 0);
-			ALUSrc: out std_logic;
-			MemWrite: out std_logic;
-			ResultSrc: out std_logic;
-			Branch: out std_logic;
-			ALUOp: out std_logic_vector(1 downto 0));
-	end component;
-	
-	component ALUDecoder 
-		port(
-			opcode_5  : in std_logic;
-			funct3    : in std_logic_vector(2 downto 0);
-			funct7_5  : in std_logic;
-			ALUOp     : in std_logic_vector(1 downto 0);
-			ALUControl: out std_logic_vector(2 downto 0)
-		);
-	end component;
-	
-	signal Branch: std_logic;
 	signal ALUOp : std_logic_vector(1 downto 0);
 begin
 
-	decoder: MainDecoder
-		port map (opcode => opcode, RegWrite => RegWrite, ImmSrc => ImmSrc,
-		ALUSrc => ALUSrc, MemWrite => MemWrite, ResultSrc => ResultSrc, Branch => Branch, ALUOp => ALUOp);
+	decoder: work.MainDecoder
+		port map (opcode => opcode, RegWrite => RegWrite, ImmSrc => ImmSrc, PCSrc => PCSrc,
+		ALUSrc => ALUSrc, MemWrite => MemWrite, ResultSrc => ResultSrc, ALUOp => ALUOp, funct3 => funct3, isZero => isZero);
 		
-	xAluDecoder: ALUDecoder
+	xAluDecoder: work.ALUDecoder
 		port map (opcode_5 => opcode(5), funct3 => funct3, funct7_5 => funct7_5, ALUOp => ALUOp,
 		ALUControl => ALUControl);
-		
-	PCSrc <= isZero and Branch;
 
 end;
--- MainDecoder.vhd
+++ MainDecoder.vhd
@@ -10,8 +10,10 @@ entity MainDecoder is
 		ALUSrc: out std_logic;
 		MemWrite: out std_logic;
 		ResultSrc: out std_logic;
-		Branch: out std_logic;
-		ALUOp: out std_logic_vector(1 downto 0));
+		ALUOp: out std_logic_vector(1 downto 0);
+		PCSrc: out std_logic;
+		isZero: in std_logic;
+		funct3: in std_logic_vector(2 downto 0));
 end;
 
 architecture Behavioural of MainDecoder is
@@ -26,31 +28,33 @@ begin
 				ALUSrc <= '1';
 				MemWrite <= '0';
 				ResultSrc <= '1';
-				Branch <= '0';
 				ALUOp <= "00";
 			when "0100011" => -- sw
 				RegWrite <= '0';
 				ImmSrc <= "01";
 				ALUSrc <= '1';
 				MemWrite <= '1';
-				Branch <= '0';
 				ALUOp <= "00";
 			when "0110011" => -- R-type instruitons
 				RegWrite <= '1';
 				ALUSrc <= '0';
 				MemWrite <= '0';
 				ResultSrc <= '0';
-				Branch <= '0';
 				ALUOp <= "10";
-			when "1100011"	=> -- beq
+			when "1100011"	=> -- beq or bne
 				RegWrite <= '0';
 				ImmSrc <= "10";
 				ALUSrc <= '0';
 				MemWrite <= '0';
-				Branch <= '1';
 				ALUOp <= "01";
+				case funct3 is 
+					when "000" => -- beq
+						PCSrc <= isZero;
+					when "001" => -- bne
+						PCSrc <= not isZero;
+					when others => 
+				end case;
 			when others =>
-				Branch <= '0';
 				RegWrite <= '0';
 				MemWrite <= '0';
 		end case;	
