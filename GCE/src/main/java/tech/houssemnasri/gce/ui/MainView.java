848c2671f602c74f53afc6f6076f86330e61f7ff
Houssem Nasri <housi.housi2015@gmail.com>
2023-05-27 19:37:32 +0100

    Implement the run/pause feature

--- GCE/src/main/java/tech/houssemnasri/gce/ui/MainView.java
+++ GCE/src/main/java/tech/houssemnasri/gce/ui/MainView.java
@@ -4,11 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
+import javafx.application.Platform;
+import javafx.beans.property.*;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -26,7 +27,6 @@ import tech.houssemnasri.gce.Program;
 import tech.houssemnasri.gce.Simulator;
 
 public class MainView extends AnchorPane implements Initializable {
-
     public static final String ADDRESS_COLUMN = "Address";
     public static final String MACHINE_CODE_COLUMN = "Machine code";
     public static final String ASSEMBLY_CODE_COLUMN = "Assembly code";
@@ -47,6 +47,8 @@ public class MainView extends AnchorPane implements Initializable {
     private final Stage stage;
     private final FileChooser fileChooser = new FileChooser();
     private final ObjectProperty<Program> programObjectProperty = new SimpleObjectProperty<>();
+    private Timer runTimer = new Timer();
+    private BooleanProperty isRunning = new SimpleBooleanProperty(false);
 
     public MainView(Simulator simulator, Stage stage) {
         loadView();
@@ -57,6 +59,7 @@ public class MainView extends AnchorPane implements Initializable {
         instructionsTableView.getSelectionModel().select(0);
         this.simulator.getMachine().PCProperty().addListener((observable, old, value) -> {
             if (value.intValue() / 4 >= instructionsTableView.getItems().size()) {
+                isRunning.set(false);
                 previousButton.setDisable(true);
                 nextButton.setDisable(true);
                 runPauseButton.setDisable(true);
@@ -73,6 +76,25 @@ public class MainView extends AnchorPane implements Initializable {
 
         initializeSidePane();
         initializeInstructionsTableView();
+
+        isRunning.addListener((obs, old, running) -> {
+            if (running) {
+                nextButton.setDisable(true);
+                previousButton.setDisable(true);
+                runTimer = new Timer();
+                runTimer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        Platform.runLater(simulator::stepIn);
+                    }
+                }, 0, 120);
+            } else {
+                nextButton.setDisable(false);
+                previousButton.setDisable(false);
+                runTimer.cancel();
+                runTimer = new Timer();
+            }
+        });
     }
 
     private void loadView() {
@@ -88,7 +110,6 @@ public class MainView extends AnchorPane implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 
     private void initializeSidePane() {
@@ -150,7 +171,11 @@ public class MainView extends AnchorPane implements Initializable {
 
     @FXML
     void runOrPauseSimulation() {
-
+        if (isRunning.get()) {
+            isRunning.set(false);
+        } else {
+            isRunning.set(true);
+        }
     }
 
     @FXML
@@ -163,6 +188,10 @@ public class MainView extends AnchorPane implements Initializable {
         previousButton.setDisable(false);
         nextButton.setDisable(false);
         runPauseButton.setDisable(false);
+        if (runTimer != null) {
+            runTimer.cancel();
+        }
         simulator.reset();
+        isRunning.set(false);
     }
 }
